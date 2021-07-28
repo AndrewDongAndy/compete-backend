3c55155f5059f9c7a4b15f4f1cd5e4f3d8ae5173
Andrew Dong <dongandrew99@gmail.com>
2021-07-28 15:41:54 -0400

    remove problem TTL

--- models/redis/problems.ts
+++ models/redis/problems.ts
@@ -7,7 +7,7 @@ import { problemsRedis } from "./redisClients";
 import { ProblemMetadata } from "../../common/interfaces/problem";
 import { PlatformName } from "../../common/interfaces/platformName";
 
-const PROBLEM_TTL = 7 * 24 * 60 * 60; // one week, in seconds
+// const PROBLEM_TTL = 7 * 24 * 60 * 60; // one week, in seconds
 
 // const isValidProblemId = (id: string) => {
 //   return /^[1-9][0-9]{3,}$/.test(id);
@@ -31,7 +31,8 @@ export const cacheProblems = async (
   let multi = problemsRedis.multi();
   for (const p of problems) {
     const key = `${p.platform}:${p.id}`;
-    multi = multi.hset(key, p).expire(key, PROBLEM_TTL);
+    multi = multi.hset(key, p);
+    // multi = multi.hset(key, p).expire(key, PROBLEM_TTL);
   }
   await multi.exec();
 };
